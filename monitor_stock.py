56d1c9ad2a848ebed90bd1b8b5d7fbdee5b7858e
Ludovic Aelbrecht <ludovic.aelbrecht@gmail.com>
2017-05-19 16:34:00 +0200

    typo bugfix + regenerated API key

--- monitor_stock.py
+++ monitor_stock.py
@@ -2,20 +2,20 @@
 from yahoo_finance import Share
 from decimal import Decimal
 import requests
+from config import EMAIL_API_URL, EMAIL_API_KEY, EMAIL_FROM, EMAIL_TO
 
 stocks = {
         'AMZN': 940,
         'TSLA': 305,
-#        'EPA:ODET': 900
+#FIXME##        'EPA:ODET': 900
         }
 
 def send_mail(item, curr_value, threshold):
     return requests.post(
-        "https://api.mailgun.net/v3/sandbox3cbc324a306047c9ac421591ff64d68d.mailgun.org/messages",
-        auth=("api", "key-6dd59912fd333e2fae06ce4f2f2f11cf"),
-        #data={"from": "Ludovic <postmaster@sandbox3cbc324a306047c9ac421591ff64d68d.mailgun.org>",
-        data={"from": "Ludovic <ludovic.aelbrecht@gmail.com>",
-              "to": "Ludovic <ludovic.aelbrecht@gmail.com>, ludovic@redhat.com",
+            EMAIL_API_URL,
+            auth=("api", EMAIL_API_KEY),
+            data={"from": EMAIL_FROM,
+              "to": EMAIL_TO,
               "subject": "==> %s stocks drop: %d <= %d" % (item, curr_value, threshold),
               "text": "https://www.google.com/finance?q=%s" % (item)})
     # You can see a record of this email in your logs: https://mailgun.com/app/logs .
@@ -26,7 +26,7 @@ def near(base_number, value):
     #lower_val = base_number - (base_number * 0.10)
     higher_val = base_number + 2
     lower_val = base_number - 2
-    
+
     if value >= lower_val and value <= higher_val:
         print("%d is close to %d (%d<->%d)" % (value, base_number, lower_val, higher_val))
         return True
@@ -67,4 +67,3 @@ for ticker, threshold in stocks.items():
     except:
         price = Decimal(Share(ticker).get_days_low())
     check_rate(ticker, curr_value=price, threshold=threshold)
-
